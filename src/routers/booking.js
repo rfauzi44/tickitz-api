eb1d26aa38fdeade4217677ccda31b2de20e758c
Rizal <rizaldifauzi44@gmail.com>
2023-01-03 08:54:14 +0700

    update

--- src/routers/booking.js
+++ src/routers/booking.js
@@ -5,7 +5,7 @@ const auth = require('../middlewares/auth');
 
 routers.get('/', auth.login, auth.isAdmin, ctrl.getBooking)
 routers.get('/my', auth.login, auth.isCustomer, ctrl.getBookingUsers)
-routers.post('/my', auth.login, auth.isCustomer, auth.isVerified, ctrl.addBooking)
+routers.post('/my', auth.login, auth.isVerified, auth.isCustomer, ctrl.addBooking)
 
 
 module.exports = routers
